f3b997cbfce483b0f44937186a6fe898ca081104
tuan <mtuan230503@gmail.com>
2025-11-13 21:39:08 +0700

    test fix css ko load 3

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\URL;
+
 abstract class Controller
 {
-    //
+    public function boot() {
+    if(config('app.env') === 'production') {
+        URL::forceScheme('https');
+    }
+}
 }
