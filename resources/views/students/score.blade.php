84202c5f1f4cb6b1d9e246285ffc7e5c8e64ee61
tuan <mtuan230503@gmail.com>
2025-11-14 09:44:37 +0700

    Them tong diem

--- resources/views/students/score.blade.php
+++ resources/views/students/score.blade.php
@@ -15,19 +15,29 @@
                     <th>Học kỳ</th>
                     <th>Điểm giữa kỳ</th>
                     <th>Điểm cuối kỳ</th>
+                    <th>Tổng điểm</th>
+                    <th>Trạng thái</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($scores as $score)
+                    @php
+                        $tong = ($score->diemgk + $score->diemck) / 2;
+                    @endphp
                     <tr>
                         <td>{{ $score->subject->tenMh ?? 'Không rõ' }}</td>
                         <td>{{ $score->hocKy }}</td>
                         <td>{{ $score->diemgk }}</td>
                         <td>{{ $score->diemck }}</td>
+                        <td>{{ number_format($tong, 2) }}</td>
+                        <td style="color: {{ $tong >= 5 ? 'green' : 'red' }};">
+                            {{ $tong >= 5 ? 'Đạt' : 'X' }}
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
+
         <a href="{{ route('panel') }}">← Quay lại</a>
     </div>
 </body>
